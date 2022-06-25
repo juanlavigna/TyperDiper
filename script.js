8eed0d43320b52370b6812723d44160b64af4ded
Juan Lavigna <juanlavigna@gmail.com>
2022-06-25 19:14:47 -0300

    Add random word to h1

--- script.js
+++ script.js
@@ -22,10 +22,26 @@ const words = [
 
 let amount = words.length
 
-function randomNumber () {
+function randomNumber (){
     return Math.floor(Math.random()*amount)
 }
 
-function randomWord () {
+function randomWord (){
     return words[randomNumber()]
+}
+
+let palabraAleatoriaH1 = document.querySelector("h1");
+
+palabraAleatoriaH1.textContent = randomWord()
+
+function time (){
+
+}
+
+function score (){
+
+}
+
+function addToDOM (){
+
 }
